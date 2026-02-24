2b5ce4f997998ef50e3a017fdf39b7315cd6621a
uyanda-maseti <masetibabalo2@gmail.com>
2026-02-24 14:57:07 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World this is Uyanda Maseti");
+        } 
+    }
+}
